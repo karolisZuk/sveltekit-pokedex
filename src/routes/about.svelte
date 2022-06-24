4737427c108c3aae45dd6c04001c43fb198c5953
JohnDoe <email@mail.com>
2022-06-24 21:56:00 +0200

    Almost done

---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>About Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+
+<p>This is a really cool Svelte Kit demo that uses Pokemon API to build a Pokedex</p>
