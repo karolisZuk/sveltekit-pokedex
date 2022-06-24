5cce94dc33372a512556439f3966fedbfbf0ecb2
JohnDoe <email@mail.com>
2022-06-24 22:01:48 +0200

    run prettier on svelte files

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-    <title>About Svelte Kit Pokedex</title>
+	<title>About Svelte Kit Pokedex</title>
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,38 @@
 <script>
-    import { pokemon } from '../stores/pokestore';
-    import PokemanCard from '../components/pokemanCard.svelte';
+	import { pokemon } from '../stores/pokestore';
+	import PokemanCard from '../components/pokemanCard.svelte';
 
-    let searchTerm = '';
-    let filteredPokemon = [];
+	let searchTerm = '';
+	let filteredPokemon = [];
 
-    $: {
-        if (searchTerm) {
-            filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()) || pokeman.id == searchTerm);
-        } else {
-            filteredPokemon = [...$pokemon];
-        }
-    }
+	$: {
+		if (searchTerm) {
+			filteredPokemon = $pokemon.filter(
+				(pokeman) =>
+					pokeman.name.trim().toLowerCase().includes(searchTerm.trim().toLowerCase()) ||
+					pokeman.id + ''.trim() === searchTerm.trim()
+			);
+		} else {
+			filteredPokemon = [...$pokemon];
+		}
+	}
 </script>
 
 <svelte:head>
-    <title>Svelte Kit Pokedex</title>
+	<title>Svelte Kit Pokedex</title>
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
-<input class="w-full rounded-md text-lg p-4 border-2" bind:value={searchTerm} type="text" placeholder="Search Pokemon">
+<input
+	class="w-full rounded-md text-lg p-4 border-2"
+	bind:value={searchTerm}
+	type="text"
+	placeholder="Search Pokemon"
+/>
 
 <div class="py-4 grid gap-4 lg:grid-cols-4 md:grid-cols-3 grid-cols-2">
-    {#each filteredPokemon as pokeman}
-        <PokemanCard pokeman={pokeman} />
-    {/each}
+	{#each filteredPokemon as pokeman}
+		<PokemanCard {pokeman} />
+	{/each}
 </div>
